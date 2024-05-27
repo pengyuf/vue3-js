ec7ad8b923f4be087a9270c2bc9956ebabd9a365
best <2907647445@qq.com>
2024-05-27 19:38:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "./store/index"
+
+
+const store = useCounterStore()
+
+store.$subscribe((mutation, state) => {
+  console.log('tore.$subscribe')
+})
+
+function changeAll() {
+  // store.$patch({
+  //   count:1,
+  //   name:'test'
+  // })
+  store.$patch((state) => {
+    state.name = 'test'
+  })
+}
+
+</script>
+
+<template>
+  <div>
+    <button @click="() => store.name = 'pneg'">改变name</button>
+    <button @click="changeAll">改变整个store</button>
+    <div>
+      {{ store.doubleCountOne }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+
+export const useStore = defineStore('main', {
+
+})
+
+// 选项式API写法
+export const useCounterStore = defineStore('counter',{
+    state:()=>({count:1}),
+    getters:{
+        doubleCount:(state)=>state.count*2,
+        /**
+         * 
+         * @returns {number}
+         */
+        doubleCountOne(){
+            return this.doubleCount+1
+        }
+    },
+    actions:{
+        increment(){
+            this.count++
+        }
+    }
+})
+
+export const useStore = defineStore('main',{
+    getters:{
+        getUserById:(state)=>{
+            return (userId)=>state.users.find((user)=>user.id === userId)
+        }
+    }
+})
+
